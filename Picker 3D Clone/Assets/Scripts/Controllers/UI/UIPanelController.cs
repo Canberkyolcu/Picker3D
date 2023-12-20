93b005369ed15be1e4c3906d415581c507bfc0a2
Canberk Yolcu <73793693+cyolcu@users.noreply.github.com>
2023-12-20 18:08:32 +0300

    Core UI

---
+++ Picker 3D Clone/Assets/Scripts/Controllers/UI/UIPanelController.cs
@@ -0,0 +1,64 @@
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanelController : MonoBehaviour
+{
+    [SerializeField] private List<Transform> layers = new List<Transform>();
+
+
+    private void OnEnable()
+    {
+        SubscribeEvents();
+    }
+
+    private void SubscribeEvents()
+    {
+        CoreUISignals.Instance.onClosePanel += OnClosePanel;
+        CoreUISignals.Instance.onOpenPanel += OnOpenPanel;
+        CoreUISignals.Instance.onCloseAllPanels += OnCloseAllPanels;
+    }
+
+    
+    private void OnClosePanel(int value)
+    {
+        if (layers[value].childCount <= 0) return;
+#if UNITY_EDITOR
+            DestroyImmediate(layers[value].GetChild(0).gameObject);
+#else
+        Destroy(layers[value].GetChild(0).gameObject);
+#endif
+    }
+
+    private void OnOpenPanel(UIPanelTypes panelTypes, int value)
+    {
+        OnClosePanel(value);
+       Instantiate(Resources.Load<GameObject>($"Screens/{panelTypes}Panel"), layers[value]);
+    }
+
+    private void OnCloseAllPanels()
+    {
+        foreach (var layer in layers)
+        {
+            if (layer.childCount <= 0) return;
+#if UNITY_EDITOR
+            DestroyImmediate(layer.GetChild(0).gameObject);
+#else
+            Destroy(layer.GetChild(0).gameObject);
+#endif
+        }
+    }
+
+    private void UnSubscribeEvents()
+    {
+        CoreUISignals.Instance.onClosePanel -= OnClosePanel;
+        CoreUISignals.Instance.onOpenPanel -= OnOpenPanel;
+        CoreUISignals.Instance.onCloseAllPanels -= OnCloseAllPanels;
+    }
+
+    private void OnDisable()
+    {
+        UnSubscribeEvents();
+    }
+}
